bd29cc0797eaf3f09dfb13c412c219ce79ae01d9
Norbert Niziołek <norbert@niziolek.dev>
2022-11-29 21:19:58 +0100

    Add official and img

---
+++ official/encode_stb.c
@@ -0,0 +1,55 @@
+#include "encode.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <stdio.h>
+#include <time.h>
+
+const char *blurHashForFile(int xComponents, int yComponents, const char *filename);
+
+int main(int argc, const char **argv)
+{
+	if (argc != 4)
+	{
+		fprintf(stderr, "Usage: %s x_components y_components imagefile\n", argv[0]);
+		return 1;
+	}
+
+	int xComponents = atoi(argv[1]);
+	int yComponents = atoi(argv[2]);
+	if (xComponents < 1 || xComponents > 8 || yComponents < 1 || yComponents > 8)
+	{
+		fprintf(stderr, "Component counts must be between 1 and 8.\n");
+		return 1;
+	}
+
+	const char *hash = blurHashForFile(xComponents, yComponents, argv[3]);
+	if (!hash)
+	{
+		fprintf(stderr, "Failed to load image file \"%s\".\n", argv[3]);
+		return 1;
+	}
+
+	printf("Blurhash:\n%s\n", hash);
+
+	return 0;
+}
+
+const char *blurHashForFile(int xComponents, int yComponents, const char *filename)
+{
+	clock_t start, end;
+	int width, height, channels;
+	start = clock();
+	unsigned char *data = stbi_load(filename, &width, &height, &channels, 3);
+	end = clock();
+	printf("Image read time: %d ms\n", (int)(((double)(end - start)) / CLOCKS_PER_SEC * 1000));
+	if (!data)
+		return NULL;
+
+	const char *hash = blurHashForPixels(xComponents, yComponents, width, height, data, width * 3);
+
+	stbi_image_free(data);
+
+	return hash;
+}
